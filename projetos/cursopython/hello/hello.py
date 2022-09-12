95fbfa79d3dbf8a78f7858a7de8b97dddcfbdc14
alexandrepaulinoss <alexandrepaulinoss@gmail.com>
2022-09-12 09:32:02 -0300

    First lines

---
+++ projetos/cursopython/hello/hello.py
@@ -0,0 +1,8 @@
+print('Hi my first Python lines!\n')
+
+print('Create an environment: python3 -m venv env')
+print('Activate the environment: source env/bin/activate')
+# print('Deactivate the environment: source env/bin/deactivate')
+print('Run: python3 filename.py')
+print('Install a package: pip install package_name')
+print('Uninstall a package: pip uninstall package_name')
